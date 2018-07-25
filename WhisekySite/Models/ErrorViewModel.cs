da45c5ee1058ea75a47510391dfab131cf85c455
Fredrik Essmander <fredrik.lg@hotmail.com>
2018-07-25 14:23:24 +0200

    Adding Repo

---
+++ WhisekySite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WhisekySite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
